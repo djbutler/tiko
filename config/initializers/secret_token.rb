87929ebde77373a74027b129140d248d421dea5d
Dan Butler <dan@tule.(none)>
2013-04-10 22:17:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiko::Application.config.secret_token = 'dd44ab64c7c9a7ac5afde6d89c02d4fead6ada20294b737a23da4987a4624dd78241c06d0a73f94ada89eeb739eebca57806356088c6fe43f1e5cd49bda488b9'
